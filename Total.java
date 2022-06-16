58012400da1c669fca2cf106daf465c58f4040aa
Kerem Serttas <keremserttas21@gmail.com>
2022-06-17 00:37:51 +0300

    Simple mapper with csv works

--- Total.java
+++ Total.java
@@ -3,11 +3,13 @@ import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -18,59 +20,70 @@ public class Total {
    * Mapper class
    **/
   public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+       extends Mapper<LongWritable, Text, Text, DoubleWritable>{
 
-    private final static IntWritable one = new IntWritable(1);
-    //private Text word = new Text();
-
-    public void map(Object key, Text value, Context context
+     @Override
+    public void map(LongWritable key, Text value, Context context
                     ) throws IOException, InterruptedException {
         String line = value.toString();
-        //CSVReader R = new CSVReader(new StringReader(line));
+
+        try{
+            if (key.get() == 0) 
+                return;
             
-//        String[] parsedLine = R.readNext();
-        String[] parsedLine = line.split(",");
-        R.close();
-        System.out.println(parsedLine);
-        // if there is no entry bigger than 2^32 use int instead
-        double duration = Double.parseDouble(parsedLine[2]);
-        Text songName = Text(parsedLine[1]);
-        System.out.println(String.format("songName: %s, duration: %f ", parsedLine[1] ,duration));
-            
-        context.write(songName,duration);
+            else{
+
+            String[] parsedLine = line.split(",");
+            System.out.println(parsedLine);
+            // if there is no entry bigger than 2^32 use int instead
+            double duration = Double.parseDouble(parsedLine[2]);
+            DoubleWritable durationWritable = new DoubleWritable(duration);
+            Text songName = new Text(parsedLine[1]);
+            System.out.println(String.format("songName: %s, duration: %f ", parsedLine[1] ,duration));
+                
+            context.write(songName,durationWritable);
+            }
+        }catch(Exception e){
+		e.printStackTrace();
+			
+	    }
     }
+
+    
+
   }
 
   /**
    *Reducer class
    *
    **/
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+  public static class DoubleSumReducer
+       extends Reducer<Text,DoubleWritable,Text,IntWritable> {
+        private DoubleWritable result = new DoubleWritable(0);
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
-    }
+        public void reduce(Text key, DoubleWritable values,
+                           Context context
+                           ) throws IOException, InterruptedException {
+
+            int sum = 0;
+         result.set(sum);
+          context.write(key, result);
+        }
+
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf,  "count");
-    job.setJarByClass(WordCount.class);
+    Job job = Job.getInstance(conf,  "Total");
+    job.setJarByClass(Total.class);
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
+//    job.setCombinerClass(DoubleSumReducer.class);
+//    job.setReducerClass(DoubleSumReducer.class);
+//    job.setOutputKeyClass(Text.class);
+//    job.setOutputValueClass(DoubleWritable.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(DoubleWritable.class);
+      FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
